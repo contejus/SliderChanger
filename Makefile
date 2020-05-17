92fc7832b41645a8da953f1d70a366a3381cfa77
contejus <22385070+contejus@users.noreply.github.com>
2020-05-16 22:28:14 -0500

    add sample preference bundle

--- Makefile
+++ Makefile
@@ -8,3 +8,5 @@ SliderChanger_FILES = Tweak.x
 SliderChanger_CFLAGS = -fobjc-arc
 
 include $(THEOS_MAKE_PATH)/tweak.mk
+SUBPROJECTS += sliderchangerprefs
+include $(THEOS_MAKE_PATH)/aggregate.mk
---
+++ sliderchangerprefs/SliderChangerRootListController.m
@@ -0,0 +1,13 @@
+#include "SliderChangerRootListController.h"
+
+@implementation SliderChangerRootListController
+
+- (NSArray *)specifiers {
+	if (!_specifiers) {
+		_specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+	}
+
+	return _specifiers;
+}
+
+@end
